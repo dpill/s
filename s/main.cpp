f8c53c241ddc8b4a5e02bb7914ba3c5c9b85db6b
dpill <a20141105059@mac05.local>
2015-11-11 21:09:11 +0800

    Initial Commit

---
+++ s/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  s
+//
+//  Created by 20141105059c on 15/11/11.
+//  Copyright © 2015年 20141105059c. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
