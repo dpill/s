fb41286cc3304f69ef2e63fa4230927f24d5366c
dpill <a20141105059@mac05.local>
2015-11-11 21:11:20 +0800

    1
    sd

--- s/main.cpp
+++ s/main.cpp
@@ -8,8 +8,13 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
     // insert code here...
-    std::cout << "Hello, World!\n";
+{   int a,*p;
+    a=100;
+    p=&a;
+    printf("a=%x &a=%x &p=%x p=%x *p=%x\n",a,&a,&p,p,*p);
     return 0;
 }
+
+
